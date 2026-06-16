dbbbf922fe4b28b9844b65c20046e72435f49648
Vincenzo Tinebra <vince.great66@gmail.com>
2026-06-16 18:47:43 +0200

    Modification de todo.md

--- app/components/tasklist/TaskList.jsx
+++ app/components/tasklist/TaskList.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-
+import { FaTrash } from 'react-icons/fa'
 import TaskItem from "../taskitem/TaskItem";
 import AddTask from "../addtask/AddTask";
 import initialData from "../../data/initialData";
@@ -13,7 +13,7 @@ export default function TaskList() {
             task.id === idTask ? { ...task, completed: !task.completed } : task
         );
         setListTasks(updatedList);
-    };
+    }
     const add = (title) => {
         const newTask = {
             id: parseInt(crypto.randomUUID().substring(0, 7), 16),
@@ -22,10 +22,17 @@ export default function TaskList() {
         };
 
         setListTasks(listTasks => [...listTasks, newTask]);
-    };
+    }
+    const deleteTasks = () => {
+        const updatedList = listTasks.filter((element) => element.completed === false);
+        setListTasks(updatedList);
+    }
     return (
         <>
             <ul className="list-group">
+                <li className="d-flex p-2">
+                    <button className="btn btn-sm ms-auto btn-outline-success me-2" onClick={deleteTasks}><FaTrash /></button>
+                </li>
                 {
                     listTasks.map((task) => {
                         return (
--- todo.md
+++ todo.md
@@ -2,3 +2,4 @@
 
 Rajouter un bouton « delete » dans le composant `TaskList` afin de supprimer toutes les tâches déjà réalisées.
 Ecrire le script de la fonction qui va supprimer toutes les tâches notée « completed » (utiliser la méthode "filter" sur la liste des tâches).
+On utilisera l'icône { FaTrash } de react-icons
